e74d679b4582e5d82373872c211265d930e1d57a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-07-13 12:07:43 -0500

    Se agregan validaciones al main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,10 +13,35 @@ public class Main {
         Vehiculo vehiculo = new Vehiculo();
         Mercancia mercancia = new Mercancia();
 
+//        Scanner lea = new Scanner(System.in);
+//        System.out.println("Digite el id del vehículo: ");
+//        vehiculo.setId(lea.nextInt());
+//        System.out.println(vehiculo.getId());
+//
+//
+//        Scanner lea = new Scanner(System.in);
+//        System.out.print("Digite el modelo del vehículo: ");
+//        vehiculo.setModelo(lea.next());
+//        System.out.println(vehiculo.getModelo());
+
         Scanner lea = new Scanner(System.in);
-        System.out.print("Digite: ");
-        vehiculo.setModelo(lea.next());
-        System.out.println(vehiculo.getModelo());
+        System.out.println("Digite la marca del vehículo:  ");
+        vehiculo.setMarca(lea.nextLine());
+        System.out.println(vehiculo.getMarca());
+
+
+        Scanner lea = new Scanner(System.in);
+        System.out.println("Digite la marca del vehículo:  ");
+        vehiculo.setMarca(lea.nextLine());
+        System.out.println(vehiculo.getMarca());
+
+        kilometraje
+
+
+        Scanner lea = new Scanner(System.in);
+        System.out.println("Digite la autonomía:  ");
+        vehiculo.setAutonomia(lea.nextInt());
+        System.out.println(vehiculo.getAutonomia());
 
 
     }
